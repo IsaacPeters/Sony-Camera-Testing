fb3e638c1c7047367c58e0e32652cbc862d3b7c9
isaacpeters <dabomb11111@gmail.com>
2018-05-15 00:43:52 -0700

    AddedSocket.io for webapp

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var SonyCamera = require('sony-camera');
 
 var cam = new SonyCamera();
 
 cam.on('update', function(param, data) {
-    console.log("updated: " + param  + " = " + data.current);
+    io.emit('update', param, value);
 });
 
-cam.on('liveviewJpeg', function(jpegBuffer) {
-    console.log(jpegBuffer);
+cam.on('liveviewJpeg', function(image) {
+    if (image) {
+        io.emit('image', image.toString('base64'));
+    }
 })
 
 cam.connect();
-cam.startViewfinder();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/static/index.html');
+});
+app.get('/jquery.js', function(req, res){
+    res.sendFile(__dirname + '/static/jquery.js');
+});
+
+io.on('connection', function(socket){
+    io.emit('params', cam.params);
+    socket.on('capture', function(){
+        cam.capture(true, function(err, name, image) {
+            if(err) {
+                return io.emit("status", "Error: " + err);
+            }
+            if(image) io.emit('image', image.toString('base64'));
+            if(name && !image) io.emit('status', "new photo: " + name);
+        });
+    });
+    socket.on('startViewfinder', function(){
+        console.log("starting liveview");
+        cam.startViewfinder();
+    });
+    socket.on('stopViewfinder', function(){
+        cam.stopViewfinder();
+    });
+    socket.on('set', function(param, value){
+        cam.set(param, value);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on localhost:3000');
+});
