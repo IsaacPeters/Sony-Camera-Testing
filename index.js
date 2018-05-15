03a317ed949d48766cd6c58406d8fb1814ded97c
isaacpeters <dabomb11111@gmail.com>
2018-05-15 00:13:09 -0700

    Added Viewfinder Test

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ cam.on('update', function(param, data) {
     console.log("updated: " + param  + " = " + data.current);
 });
 
+cam.on('liveviewJpeg', function(jpegBuffer) {
+    console.log(jpegBuffer);
+})
+
 cam.connect();
+cam.startViewfinder();
